4fe4948bb0e04eb901b9138a289f053f19b6c2c4
sbonnaud1 <serge.bonnaud@fr.ibm.com>
2020-04-11 19:32:07 +0200

    Update config_dev.js

--- js/config_dev.js
+++ js/config_dev.js
@@ -1,8 +1,8 @@
-var org_id           = "abcd1z";             // replace with your organization ID
+var org_id           = "uz6g30";             // replace with your organization ID
 var domain           = "internetofthings.ibmcloud.com";
 var device_type      = "MotionDevice";       // replace with your device type ID
 var device_id        = "my-phone";           // replace with your device ID
-var dev_auth_token   = "$9HjMsPk8w1jVb7knd"; // replace with your device token
+var dev_auth_token   = "_mOjoj4Quk4PhJ@Vt9"; // replace with your device token
 var qos              = 2; // see
 // <https://www.ibm.com/support/knowledgecenter/SSQP8H/iot/platform/reference/mqtt/index.html>
 
